fc753b8a6720f420f3a849a79ad712833ced4316
Li Chaoyi <icylydia@gmail.com>
2017-09-04 16:46:24 +0900

    single page application

---
+++ disneyNavigationPreview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  disneyNavigationPreview
+//
+//  Created by ebuser on 2017/9/4.
+//  Copyright © 2017年 ebuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
