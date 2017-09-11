19260770e332e139dd65b4e3b4017ffca80c84f0
Li Chaoyi <icylydia@gmail.com>
2017-09-11 16:13:32 +0900

    tab bar update

--- disneyNavigationPreview/Attraction/AttractionVC.swift
+++ disneyNavigationPreview/Attraction/AttractionVC.swift
@@ -8,28 +8,25 @@
 
 import UIKit
 
-class AttractionVC: UIViewController {
+class AttractionVC: UIViewController, Localizable {
+
+    let localizeFileName = "attraction"
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        setupNavigation()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setupNavigation() {
+        title = localize(for: "")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
