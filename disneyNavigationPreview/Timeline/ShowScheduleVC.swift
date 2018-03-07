3ee9a13415fda01e2bda4da674abf7069718195f
Li Chaoyi <icylydia@gmail.com>
2018-03-07 19:18:06 +0900

    no comment

--- disneyNavigationPreview/Timeline/ShowScheduleVC.swift
+++ disneyNavigationPreview/Timeline/ShowScheduleVC.swift
@@ -284,25 +284,6 @@ extension ShowScheduleVC: UICollectionViewDelegateTimeLineLayout, UICollectionVi
             let actionCancel = UIAlertAction(title: localize(for: "Cancel"), style: .cancel, handler: nil)
             alertController.addAction(actionDetail)
 
-            // 收藏
-            let actionFavorite = UIAlertAction(title: localize(for: "Save favorite"),
-                                               style: .default,
-                                               handler: { [unowned self] _ in
-                                                let favorite = DB.FavoriteModel(park: self.park,
-                                                                                str_id: event.id,
-                                                                                lang: .cn,
-                                                                                name: event.name,
-                                                                                thum: event.thum,
-                                                                                area: event.area,
-                                                                                tintColor: event.tintColor.hex)
-                                                DB.insert(favorite: favorite)
-            })
-            // 取消收藏
-            let actionCancelFavorite = UIAlertAction(title: localize(for: "Cancel favorite"),
-                                                     style: .default,
-                                                     handler: { _ in
-                                                        DB.delete(favoriteId: event.id)
-            })
             // 检查提醒是否已经设置
             if DB.exists(alarmIdentifier: event.identifier) {
                 alertController.addAction(actionCancelAlarm)
@@ -311,12 +292,6 @@ extension ShowScheduleVC: UICollectionViewDelegateTimeLineLayout, UICollectionVi
                 alertController.addAction(actionAlarm)
             }
 
-            // 检查是否已经收藏
-            if DB.exists(favoriteId: event.id) {
-                alertController.addAction(actionCancelFavorite)
-            } else {
-                alertController.addAction(actionFavorite)
-            }
 
             alertController.addAction(actionCancel)
             present(alertController, animated: true, completion: nil)
